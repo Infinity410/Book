b33dcf53dccd3ad494cbbbdc5f7e121cdb254244
Infinity410 <444152364@qq.com>
2020-11-02 16:29:43 +0800

    ver0.1.1

--- src/com/example/manage/Test.java
+++ src/com/example/manage/Test.java
@@ -4,13 +4,6 @@ package com.example.manage;
 public class Test {
     public static void main(String[] args) {
         User u1 =new User();
-        String st="2019-12-20";
-        u1.setSerialNo(1);
-        u1.setID("1998017");
-        u1.setNo("1014");
-        u1.setType(1);
-        u1.setOperator("1988003");
-        u1.setDatel(st);
         System.out.println(u1);
         UserManagement management = new UserManagement();
         management.addview();
